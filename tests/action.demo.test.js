eab14a0af282dd48fdff6c406e2f0adaf5ccf6af
Jeevan <jeevanauji1431@gmail.com>
2025-10-31 15:08:14 +0530

    update new test

--- tests/action.demo.test.js
+++ tests/action.demo.test.js
@@ -3,8 +3,15 @@ fixture("Action Demo Tests")
 
 test("TestCafe Action", async t => {
     await t
+
+        //click
         .click("body > div.navbar > a:nth-child(1)")
         .wait(2000)
-        .click("body > div.navbar > a:nth-child(2)");
-
+        .click("body > div.navbar > a:nth-child(2)")
+        .wait(2000)
+        // typeText
+        .typeText("#fname", "Jeevan")
+        .wait(2000)
+        .typeText("#lname", "Auji")
+        .wait(2000)
 });
