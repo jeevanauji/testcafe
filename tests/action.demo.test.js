aa2bf8fd93eeee922cea228fd3a6a2a6f35912f9
Jeevan <jeevanauji1431@gmail.com>
2025-10-31 16:07:40 +0530

    update new test

--- tests/action.demo.test.js
+++ tests/action.demo.test.js
@@ -1,3 +1,5 @@
+const { Selector } = require("testcafe");
+
 fixture("Action Demo Tests")
     .page("https://trytestingthis.netlify.app/");
 
@@ -14,4 +16,32 @@ test("TestCafe Action", async t => {
         .wait(2000)
         .typeText("#lname", "Auji")
         .wait(2000)
+        // doubleClick
+        .doubleClick("body > div.row > div.side.ex1 > button")
+        .wait(2000)
+        // rightClick
+        .rightClick("#fname")
+        .expect(Selector('#cell-popup-menu').exists).notOk()
+        .wait(2000)
+
+        // hover
+        .hover(".tooltip")
+        .wait(2000)
+
+        // drag and drop
+        .dragToElement("#drag1", "#div1")
+        .wait(2000)
+
+        // scroll to element
+        .scrollIntoView('body > div.footer > h4:nth-child(4) > a')
+        .wait(2000)
+
+        //alert
+        .setNativeDialogHandler(() => true)
+        .click('body > div.row > div.side.ex1 > div.pop-up-alert > button')
+        .wait(2000)
+        
+        //screenshot
+        .takeScreenshot('screenshot.png')
+        .wait(2000);
 });
